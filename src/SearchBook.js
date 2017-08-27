82ffa3f3f25bdd9e4f5ba54a276b33f1fb2793ff
Fernando Ordas <fernando.ordas@yapiko.com>
2017-08-27 14:33:15 +0200

    fix: Fix back link in search book screen

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchBook extends React.Component {
 
@@ -6,7 +7,7 @@ class SearchBook extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -21,7 +22,9 @@ class SearchBook extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+
+          </ol>
         </div>
       </div>
     )
